c60590227f1eca89eb36afbfd22e729cd3e78e31
Naga Chaitanya Konada <knc@outlook.in>
2020-08-20 14:54:01 +0530

    added a contact us form

--- src/App.js
+++ src/App.js
@@ -5,7 +5,35 @@ export default function App() {
   return (
     <div className="App">
       <h1>Contact me</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <form name="contact-naga" method="POST">
+        <input type="hidden" name="form-name" value="contact-naga" />
+        <div>
+          <label htmlFor="name">
+            <strong>Name: </strong>
+            <input id="name" name="name" placeholder="your name..." />
+          </label>
+        </div>
+        <div>
+          <label htmlFor="title">
+            <strong>Title: </strong>
+            <input id="title" name="title" placeholder="what is it about..." />
+          </label>
+        </div>
+        <div>
+          <label htmlFor="message">
+            <strong>Message: </strong>
+            <br />
+            <textarea
+              id="message"
+              name="message"
+              placeholder="tell me how I can help..."
+            />
+          </label>
+        </div>
+        <div>
+          <button type="submit">Send</button>
+        </div>
+      </form>
     </div>
   );
 }
