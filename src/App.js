8048284bf20e0ba771adeb526d99f2e07e1e9acd
nagachaitanya.konada <nagachaitanya.konada@servicenow.com>
2021-02-20 14:20:16 +0530

    ui changes to contact form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import "./App.css";
 
-const encode = data => {
+const encode = (data) => {
   return Object.keys(data)
-    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .map((key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
     .join("&");
 };
 
@@ -32,7 +32,7 @@ function App() {
   const [submitted, setSubmitted] = React.useState(false);
   const [state, dispatch] = React.useReducer(contactFormReducer, defaultState);
   const firstElementRef = React.useRef();
-  const handleSubmit = e => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     setSubmitted(false);
     fetch(`/`, {
@@ -47,7 +47,7 @@ function App() {
     }
   };
 
-  const handleChange = e => {
+  const handleChange = (e) => {
     setSubmitted(false);
     e.persist();
     dispatch({
@@ -60,74 +60,82 @@ function App() {
   console.log("appp is initializing");
 
   return (
-    <div className="App">
-      <h1>Contact Form</h1>
-      {submitted && (
-        <div>
-          <em>Form submitted successfully</em>
+    <div className="app">
+      <h1 className="app__title">Contact me</h1>
+      {submitted ? (
+        <div className="app__success">
+          <em>Success!</em>
+          <p>Thanks for contacting me.</p>
+          <p>Now, sit back and relax, I will reach out to you soon.</p>
+        </div>
+      ) : (
+        <div className="app__form">
+          <form name="contact-naga" method="POST" onSubmit={handleSubmit}>
+            <input type="hidden" name="form-name" value="contact-naga" />
+            <div className="app__field-control">
+              <label htmlFor="name">
+                <strong>name</strong>
+                <br />
+                <input
+                  ref={firstElementRef}
+                  id="name"
+                  name="name"
+                  type="text"
+                  placeholder="your name..."
+                  value={state.name}
+                  onChange={handleChange}
+                />
+              </label>
+            </div>
+            <div className="app__field-control">
+              <label htmlFor="title">
+                <strong>subject</strong>
+                <br />
+                <input
+                  id="title"
+                  name="title"
+                  value={state.title}
+                  placeholder="what is it about..."
+                  onChange={handleChange}
+                />
+              </label>
+            </div>
+            <div className="app__field-control">
+              <label htmlFor="email">
+                <strong>email</strong>
+                <br />
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  value={state.email}
+                  placeholder="so I can reach you..."
+                  onChange={handleChange}
+                />
+              </label>
+            </div>
+            <div className="app__field-control">
+              <label htmlFor="message">
+                <strong>message</strong>
+                <br />
+                <textarea
+                  id="message"
+                  name="message"
+                  value={state.message}
+                  placeholder="tell me how I can help..."
+                  onChange={handleChange}
+                  rows="10"
+                />
+              </label>
+            </div>
+            <div>
+              <button className="app__submit" type="submit">
+                Send
+              </button>
+            </div>
+          </form>
         </div>
       )}
-      <form name="contact-naga" method="POST" onSubmit={handleSubmit}>
-        <input type="hidden" name="form-name" value="contact-naga" />
-        <div>
-          <label htmlFor="name">
-            <strong>Name: </strong>
-            <br />
-            <input
-              ref={firstElementRef}
-              id="name"
-              name="name"
-              type="text"
-              placeholder="your name..."
-              value={state.name}
-              onChange={handleChange}
-            />
-          </label>
-        </div>
-        <div>
-          <label htmlFor="title">
-            <strong>Title: </strong>
-            <br />
-            <input
-              id="title"
-              name="title"
-              value={state.title}
-              placeholder="what is it about..."
-              onChange={handleChange}
-            />
-          </label>
-        </div>
-        <div>
-          <label htmlFor="email">
-            <strong>Email: </strong>
-            <br />
-            <input
-              id="email"
-              name="email"
-              type="email"
-              value={state.email}
-              placeholder="so I can reach you..."
-              onChange={handleChange}
-            />
-          </label>
-        </div>
-        <div>
-          <label htmlFor="message">
-            <strong>Message: </strong>
-            <br />
-            <textarea
-              id="message"
-              name="message"
-              value={state.message}
-              placeholder="tell me how I can help..."
-              onChange={handleChange}
-            />
-          </label>
-        </div>
-        <div>
-          <button type="submit">Send</button>
-        </div>
-      </form>
     </div>
   );
 }
